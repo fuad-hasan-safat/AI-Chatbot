1e1f03ad7d55c49165d9318db9210285a71a4892
fuadsaft <fuadsafat16@gmail.com>
2023-11-23 09:13:54 +0600

    Signup validation added

--- Backend/src/routes/user-routes.ts
+++ Backend/src/routes/user-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getAllUsers, usersSignup } from "../controller/user-controller.js";
+import { signupValidator, validate } from "../utils/validators.js";
 
 const useRoutes = Router();
 useRoutes.get('/', getAllUsers);
-useRoutes.post('/signup', usersSignup);
+useRoutes.post('/signup', validate(signupValidator), usersSignup);
 export default useRoutes;
