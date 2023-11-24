0a6de71403515301e7310aa05f54a80c938f0de6
fuadsaft <fuadsafat16@gmail.com>
2023-11-24 09:58:37 +0600

    Update App.tsx
    Setting routes of the pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Chat from './pages/Chat';
+import NotFound from './pages/NotFound';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <main>hellow world</main>
+     <main>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+     </main>
   )
 }
 
