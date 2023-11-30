348ce7031622e625f468fe64a84b3a93ff895252
fuadsaft <fuadsafat16@gmail.com>
2023-12-01 00:05:41 +0600

    Update chat-controllers.ts

--- Backend/src/controller/chat-controllers.ts
+++ Backend/src/controller/chat-controllers.ts
@@ -1,9 +1,32 @@
 import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
 
-export const generateChatCompletion =async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+export const generateChatCompletion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    const { message } = req.body;
+  const { message } = req.body;
+  const user = await User.findById(res.locals.jwtData.id);
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "User not registered or Token malfunctioned" });
+  }
+
+  // grab chats of user
+  // const chats = user.chats.map(({role, content }) => ({role, content}));
+  const chats = user.chats.map((item) => {
+    
+    return {
+      role: item.role,
+      content: item.content,
+    };
+  });
+
+
+
+  // send all chats with new one to openai API
+
+  // get lattest response
 };
